5d3a01271150480092dacbacf4cd553c489f53d1
AI Kiwi <contact@aikiwi.dev>
2022-06-27 10:45:30 +1200

    fixed up to ask where to insatall (code untested tho)

--- install.lua
+++ install.lua
@@ -1,2 +1,9 @@
-shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/cc-Music/main/startup.lua startup.lua")
-shell.run("startup.lua")
+print("where would you like it to be installed? (nothing for auto start)")
+local Path = read("")
+
+if Path == "" then
+  Path = "startup.lua"
+end
+
+shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/cc-Music/main/core.lua " .. Path)
+shell.run(Path)
