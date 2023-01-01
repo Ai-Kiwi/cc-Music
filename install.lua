1b6f2f45397c2e7cbda4723fde77eb6a6a5b8a8e
AI Kiwi <contact@aikiwi.dev>
2023-01-01 14:20:31 +1300

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,2 +1,2 @@
-shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/cc-Music/main/startup.lua startup.lua")
+shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/cc-Music/main/core.lua startup.lua")
 shell.run("startup.lua")
